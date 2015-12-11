e6cac9c89cc06b687415dba9e634007d0f458ec3
Charlie Koster <ckoster22@gmail.com>
2015-12-11 12:44:13 -0600

    Upgraded Cesium

---
+++ README.md
@@ -0,0 +1,7 @@
+# Running the example
+
+`npm install`
+
+`npm start`
+
+Navigate to http://locathost:3001/
--- src/js/cesium.patcher.js
+++ src/js/cesium.patcher.js
@@ -8,11 +8,11 @@ export default {
             let nextCity = nextState.cities[index];
 
             if (currCity.visible !== nextCity.visible) {
-                patches.push(Patch.patch({
+                patches.push({
                     id: currCity.id,
                     attribute: 'visible',
                     nextValue: nextCity.visible
-                }));
+                });
             }
             // else if (currCity.name !== nextCity.name) { .. and so on .. }
         });
